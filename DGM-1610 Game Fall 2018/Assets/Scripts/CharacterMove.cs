b5d05c243fbe427cc28e02916125778fdb2474c8
Unknown <micah@codepoet.org>
2018-09-10 17:14:22 -0600

    Script adding

---
+++ DGM-1610 Game Fall 2018/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	public int MoveSpeed;
+	private float JumpHeight;
+
+	// Use this for initialization
+	void Start () {
+		print("Hello world?");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
