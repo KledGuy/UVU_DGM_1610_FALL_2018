f89f808e21a5969837de17c19c3aa388ad736ce9
Unknown <micah@codepoet.org>
2018-10-03 17:14:15 -0600

    More Scripts

---
+++ DGM-1610 Game Fall 2018/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Controller2D Player;
+
+	//Particles, man
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	//Delay
+	public float RespawnDelay;
+
+	//Penalty
+	public int PointPenaltyOnDeath;
+
+	//Gravity Value
+	private float GravityStore;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void RespawnPlayer() {
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo() {
+		//Death Particle
+		Instantiate (deathParticle. player.transform.position, player.transform.rotation;);
+		//Hide Player
+		player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
+		//Gravity Reset
+		gravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
+		player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		//Point Penalty
+		ScoreManager.AddPoints(-pointPenaltyOnDeath);
+		//Debug Message
+		Debug.Log ("Player Respawn");		
+	}
+}
---
+++ DGM-1610 Game Fall 2018/Assets/Scripts/quiz.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quiz : MonoBehaviour {
+
+	//Variables
+	public int Var1;
+	public int Var2;
+	int total;
+
+	//Addition Function
+	public int func(int Var1,int Var2) {
+		total = Var1 + Var2;
+		print(total);
+		return total;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		func(1,3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
