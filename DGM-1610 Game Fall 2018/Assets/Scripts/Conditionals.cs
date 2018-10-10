d0801938a3c0819d12d9b5b6d1d4b55d746b8314
Unknown <micah@codepoet.org>
2018-10-10 17:14:14 -0600

    KillPlayer

---
+++ DGM-1610 Game Fall 2018/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour {
+	
+	public string Weather;
+
+	// Use this for initialization
+	void Start () {
+		if(Weather == "Raining")
+			print("Weather is " + Weather);
+		else if(Weather == "Snowing")
+			print("Weather is " + Weather);	
+		else if(Weather == "Hailing")
+			print("Weather is " + Weather);		
+		else if(Weather == "Blowing")
+			print("Weather is " + Weather);	
+		else
+			print("This weather is unknown");
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
