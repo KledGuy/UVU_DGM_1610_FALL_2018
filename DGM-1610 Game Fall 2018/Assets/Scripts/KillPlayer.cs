17d2fef30b92a4b89ce94b259b8892c4d8c4a836
Unknown <micah@codepoet.org>
2018-10-10 17:16:10 -0600

    Small fix

--- DGM-1610 Game Fall 2018/Assets/Scripts/KillPlayer.cs
+++ DGM-1610 Game Fall 2018/Assets/Scripts/KillPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KillPlayer : MonoBehaviour {
 
-	public LevelManager : LevelManager
+	public LevelManager LevelManager;
 
 	// Use this for initialization
 	void Start () {
