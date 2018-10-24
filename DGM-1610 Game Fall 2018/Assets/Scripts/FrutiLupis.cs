5870f6e46a3ae792ec89d46f85f8fe3cefe8f448
Unknown <micah@codepoet.org>
2018-10-24 17:14:35 -0600

    Fixes and Loops

---
+++ DGM-1610 Game Fall 2018/Assets/Scripts/FrutiLupis.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrutiLupis : MonoBehaviour {
+
+	public int Numero = 40;
+
+	public string Color = "Blue";
+
+	public string Pennies = "Twenty"
+
+	// Use this for initialization
+	void Start () {
+		//for(int i=0; i <= 100; i++){
+
+			//print(i + " Ants Marching");
+
+
+		//}
+
+		//while(Numero >= 0){
+			//print("Countdown " + Numero);
+			//Numero --;
+
+		//}
+
+		for(int i=20; i >= 00; i--){
+
+			print(i + " Numbers");
+
+		}
+
+		while(Color == "Red"){
+			print("Color equals " + Color);
+			
+
+		}
+
+		for(int i=37; i >= 00; i--){
+
+			print("Totals = " + i);
+
+		}
+
+		while(Pennies == "Twenty"){
+			print("Take a penny ");
+			Pennies = "Nineteen"
+			
+
+		}
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
