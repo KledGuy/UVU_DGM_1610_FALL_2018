0c16c7c1eb0106987ee196c34af5a2928ed6b46d
Unknown <micah@codepoet.org>
2018-11-07 17:18:07 -0700

    Enums

--- DGM-1610 Game Fall 2018/Assets/Scripts/Homework Scripts/Arrays.cs
+++ DGM-1610 Game Fall 2018/Assets/Scripts/Homework Scripts/Arrays.cs
@@ -9,6 +9,14 @@ public class Arrays : MonoBehaviour {
 
 	public string[] Cheese;
 
+	public string[] Coins;
+
+	public int[] Puzzles = new int[4];
+
+	public string[] Poodles;
+
+	public int[] Handedness = new int[4];
+
 
 
 	// Use this for initialization
@@ -29,7 +37,7 @@ public class Arrays : MonoBehaviour {
 
 		print(Eggscellent[8]);
 
-		Cheese = new string[4];
+		Cheese = new string[3];
 
 		Cheese[0] = "Gouda";
 		Cheese[1] = "Wensleydale";
@@ -39,6 +47,39 @@ public class Arrays : MonoBehaviour {
 		foreach(string item in Cheese){
 			print ("Chese type " + item);
 		}
+
+		Coins = new string[5];
+
+		Coins[0] = "Pennies";
+		Coins[1] = "Nickles";
+		Coins[2] = "Dimes";
+		Coins[3] = "Quarters";
+		Coins[4] = "Half Dollar";
+		Coins[5] = "Dollar";
+
+		Puzzles[0] = 5;
+		Puzzles[1] = 4;
+		Puzzles[2] = 3;
+		Puzzles[3] = 2;
+		Puzzles[4] = 1;
+
+		Poodles[0] = "Type 1";
+		Poodles[1] = "Type 2";
+		Poodles[2] = "Type 3";
+		Poodles[3] = "Type 4";
+		Poodles[4] = "Type 5";
+
+		foreach(string item in Poodles){
+			print("Poodles of " + item);
+		}
+
+
+		Handedness[0] = 1;
+		Handedness[1] = 2;
+		Handedness[2] = 1;
+		Handedness[3] = 1;
+		Handedness[4] = 3;
+
 
 	}
 	
---
+++ DGM-1610 Game Fall 2018/Assets/Scripts/Homework Scripts/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enums : MonoBehaviour {
+
+	enum Inventory {chinese, italian, thai, jamaican, mexican, egyptian, english, korean}
+
+
+	// Use this for initialization
+	void Start () {
+
+		print(Inventory.chinese);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
